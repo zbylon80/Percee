3efd3a74f7e4e3786195b7971ef6158cac8866e6
Zbigniew <zbylon80@gmail.com>
2025-02-18 20:49:28 +0100

    added helper functions for updatePayloads function

--- utils/upgradePayloads.ts
+++ utils/upgradePayloads.ts
@@ -3,47 +3,40 @@ import { adjustDate } from './dateUtils';
 import { updateJsonFile } from './jsonUtils';
 import { getPreviousWeekDates } from './dateUtils';
 
+const updateAlgFiles = (algFiles: string[], sDateBeginOffsetDays: number, sDateBeginOffsetHours: number, sDateEndOffsetDays: number, sDateEndOffsetHours: number) => {
+    algFiles.forEach((algFile) => {
+        updateJsonFile(path.resolve(__dirname, `../testData/${algFile}.json`), (data) => {
+            data.body = data.body
+                .replace(/sDateBegin:\s*'[^']+'/g, `sDateBegin:'${adjustDate(sDateBeginOffsetDays, sDateBeginOffsetHours, 'sql')}'`)
+                .replace(/sDateEnd:\s*'[^']+'/g, `sDateEnd:'${adjustDate(sDateEndOffsetDays, sDateEndOffsetHours, 'sql')}'`);
+        });
+    });
+};
+
+const updateAnaFiles = (anaFiles: string[], beginDateOffsetDays: number, beginDateOffsetHours: number, endDateOffsetDays: number, endDateOffsetHours: number) => {
+    anaFiles.forEach((anaFile) => {
+        updateJsonFile(path.resolve(__dirname, `../testData/${anaFile}.json`), (data) => {
+            Object.assign(data.analysisRangeModel, {
+                beginDate: adjustDate(beginDateOffsetDays, beginDateOffsetHours, 'iso'),
+                endDate: adjustDate(endDateOffsetDays, endDateOffsetHours, 'iso'),
+            });
+        });
+    });
+};
+
 export const updatePayloads = (testType: string, algFiles: string[], anaFiles: string[]) => {
     const { previousMondayOffset, lastSundayOffset } = getPreviousWeekDates();
-    const midnight = -new Date().getUTCHours();
+    const midnightUTCOffset = -new Date().getUTCHours();
 
     switch (testType) {
         case '15m':
-            algFiles.forEach((algFile) => {
-                updateJsonFile(path.resolve(__dirname, `../testData/${algFile}.json`), (data) => {
-                    data.body = data.body
-                        .replace(/sDateBegin:\s*'[^']+'/g, `sDateBegin:'${adjustDate(0, -2, 'sql')}'`)
-                        .replace(/sDateEnd:\s*'[^']+'/g, `sDateEnd:'${adjustDate(0, 0, 'sql')}'`);
-                });
-            });
-
-            anaFiles.forEach((anaFile) => {
-                updateJsonFile(path.resolve(__dirname, `../testData/${anaFile}.json`), (data) => {
-                    Object.assign(data.analysisRangeModel, {
-                        beginDate: adjustDate(0, -3, 'iso'),
-                        endDate: adjustDate(0, -1, 'iso'),
-                    });
-                });
-            });
+            updateAlgFiles(algFiles, 0, -2, 0, 0);
+            updateAnaFiles(anaFiles, 0, -3, 0, -1);
             break;
 
         case '1w':
-            algFiles.forEach((algFile) => {
-                updateJsonFile(path.resolve(__dirname, `../testData/${algFile}.json`), (data) => {
-                    data.body = data.body
-                        .replace(/sDateBegin:\s*'[^']+'/g, `sDateBegin:'${adjustDate(previousMondayOffset, midnight, 'sql')}'`)
-                        .replace(/sDateEnd:\s*'[^']+'/g, `sDateEnd:'${adjustDate(lastSundayOffset, midnight, 'sql')}'`);
-                });
-            });
-
-            anaFiles.forEach((anaFile) => {
-                updateJsonFile(path.resolve(__dirname, `../testData/${anaFile}.json`), (data) => {
-                    Object.assign(data.analysisRangeModel, {
-                        beginDate: adjustDate(previousMondayOffset, midnight - 1, 'iso'),
-                        endDate: adjustDate(lastSundayOffset, midnight - 1, 'iso'),
-                    });
-                });
-            });
+            updateAlgFiles(algFiles, previousMondayOffset, midnightUTCOffset, lastSundayOffset, midnightUTCOffset);
+            updateAnaFiles(anaFiles, previousMondayOffset, midnightUTCOffset - 1, lastSundayOffset, midnightUTCOffset - 1);
             break;
 
         default:
@@ -51,4 +44,3 @@ export const updatePayloads = (testType: string, algFiles: string[], anaFiles: s
             break;
     }
 };
-
