7713e49582351a88391a82cf97b39c1427c31863
Zbigniew <zbylon80@gmail.com>
2025-02-19 07:49:05 +0100

    unnecessary export removed

--- utils/jsonUtils.ts
+++ utils/jsonUtils.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { adjustDate } from './dateUtils';
 
-export const updateJsonFile = (filePath: string, updateFn: (data: any) => void) => {
+const updateJsonFile = (filePath: string, updateFn: (data: any) => void) => {
     const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
     updateFn(data);
     fs.writeFileSync(filePath, JSON.stringify(data, null, 4), 'utf-8');
